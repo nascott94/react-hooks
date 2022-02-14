5a4f03122b4e4bd032ac18d6fa2b576bad958370
nascott <nascott94@outlook.com>
2022-02-14 12:29:55 -0700

    using state hook to add items to list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ function App() {
   //state hook
   //useState is a Hook that lets you add React state to function components.
   const [newItem, setNewItem] = useState('');
+  //empty array that will hold all newitems
+  const [items, setItems] = useState([]);
+
+  //helper functions
+  function addItem() {
+    if (!newItem) {
+      alert('plz enter something!');
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+    setItems((oldList) => [...oldList, item]);
+    setNewItem('');
+
+    console.log(items);
+  }
 
   return (
     <div className="App">
@@ -19,9 +38,14 @@ function App() {
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button>Add</button>
+      <button onClick={() => addItem()}>Add</button>
 
       {/* 3. list of items (unordered list with items) */}
+      <ul>
+        {items.map((item) => {
+          return <li key={item.id}>{item.value}</li>;
+        })}
+      </ul>
     </div>
   );
 }
